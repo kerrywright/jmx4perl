c30872d3f07634834dbace15cb05ccc598d513ee
Roland Huß <roland@tichny.org>
2010-04-05 17:32:51 +0200

    Worked on, added 'desc' for MBean info to the ListHandler. Protocol Version now take a minor.major approach.

--- agent/modules/j4p-jar/src/main/java/org/jmx4perl/Version.java
+++ agent/modules/j4p-jar/src/main/java/org/jmx4perl/Version.java
@@ -34,8 +34,8 @@ public final class Version {
 
     private static final String VERSION = "0.65.0";
 
-    // Version of the 
-    private static final String PROTOCOL = "3";
+    // Major.Minor version of protocol
+    private static final String PROTOCOL = "3.1";
 
     private Version() {}
 
